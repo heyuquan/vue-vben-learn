0d977bb3ab5a5e07e757047cf646fb4b2e816912
heyuquan <369220123@qq.com>
2020-11-26 18:15:28 +0800

    1

--- build/utils.ts
+++ build/utils.ts
@@ -101,3 +101,29 @@ export interface ViteEnv {
   VITE_DROP_CONSOLE: boolean;
   VITE_BUILD_GZIP: boolean;
 }
+
+export function loadEnv(): ViteEnv {
+  const env = process.env.NODE_ENV;
+  const ret: any = {};
+  const envList = [`.env.${env}.local`, `.env.${env}`, '.env.local', '.env'];
+  envList.forEach((e) => {
+    dotenv.config({ path: e });
+  });
+
+  for (const envName of Object.keys(process.env)) {
+    let realName = (process.env as any)[envName].replace(/\\n/g, '\n');
+    realName = realName === 'true' ? true : realName === 'false' ? false : realName;
+    if (envName === 'VITE_PORT') {
+      realName = Number(realName);
+    }
+    if (envName === 'VITE_PROXY') {
+      try {
+        realName = JSON.parse(realName);
+      } catch (error) {}
+    }
+    ret[envName] = realName;
+    process.env[envName] = realName;
+  }
+
+  return ret;
+}
