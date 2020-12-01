b0bde4e9af54712d6443a59d1ec0aeec38c93636
369220123@qq.com <369220123@qq.com>
2020-12-01 23:51:08 +0800

    1

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -6,3 +6,7 @@ import store from '/@/store';
 import { PROJ_CFG_KEY, LOCK_INFO_KEY } from '/@/enums/cacheEnum';
 
 import { hotModuleUnregisterModule } from '/@/utils/helper/vuexHelper';
+import { setLocal, getLocal, removeLocal } from '/@/utils/helper/persistent';
+import { deepMerge } from '/@/utils';
+
+//import { userStore } from './user';
--- src/utils/storage/index.ts
+++ src/utils/storage/index.ts
@@ -1,1 +1,16 @@
-import { getSto } from '/@/utils/helper/envHelper';
+import { getStorageShortName } from '/@/utils/helper/envHelper';
+import { createStorage as create } from './storage';
+
+const createOptions = (storage = sessionStorage) => {
+  return {
+    storage,
+    prefixkey: getStorageShortName,
+  };
+};
+
+export const WebStorage = create(createOptions());
+
+export const createStorage = (storage: Storage = sessionStorage) => {
+  return create(createOptions(storage))!;
+};
+export default WebStorage;
